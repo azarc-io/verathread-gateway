50d3ed288d02339d1788e0c7283e12857c64afaa
Wael <wael.jammal@azarc.io>
2024-07-02 17:00:46 +0100

    VTHP-1940: Compose dev env

--- cmd/web2/src/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-import useFederatedComponent from './useFederatedComponent';
-export { useFederatedComponent };
-export default useFederatedComponent;
-
-export type { IFederatedComponent } from './types';
