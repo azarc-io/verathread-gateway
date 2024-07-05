97a39392fde973b222f8bb62c711988977e589de
Wael <wael.jammal@azarc.io>
2024-07-05 16:54:44 +0000

    Update dev container

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -12,25 +12,18 @@ git config pull.rebase true
 git clone https://github.com/azarc-io/verathread-dev-toolkit.git /workspaces/verathread-dev-toolkit
 git -C /workspaces/verathread-dev-toolkit pull
 
-## add go bin path to rc
-echo 'GOPRIVATE=github.com/azarc-io' >> ~/.zshrc
-echo 'GOPRIVATE=github.com/azarc-io' >> ~/.bashrc
-
 ## install task.dev
 go install github.com/go-task/task/v3/cmd/task@latest
 
 ## run setup task
 task setup
 
-## update hosts file
-echo 127.0.0.1 dev.cluster.local | sudo tee -a /etc/hosts
-echo 127.0.0.1 k3d-local-registry | sudo tee -a /etc/hosts
-
 ## spin up k3d from the toolkit
 pushd /workspaces/verathread-dev-toolkit
 task setup
 task k3d:create
-kubectl wait node --all --for condition=ready --timeout=600s
+kubectl wait node --all --for condition=ready --timeout=120s
+sleep 15
 task k3d:install:charts
 popd
 
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,5 +4,12 @@
 
 echo "post-start start"
 
+## update hosts file
+echo 127.0.0.1 dev.cluster.local | sudo tee -a /etc/hosts
+echo 127.0.0.1 k3d-local-registry | sudo tee -a /etc/hosts
+
+## add go bin path to rc
+echo 'GOPRIVATE=github.com/azarc-io' >> ~/.zshrc
+echo 'GOPRIVATE=github.com/azarc-io' >> ~/.bashrc
 
 echo "post-start complete"
