eb7c71ca35e1e0aa322710f49d67ee6d396e1889
Wael <wael.jammal@azarc.io>
2024-07-05 17:00:53 +0000

    Update dev contianer

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -8,6 +8,14 @@ echo "on-create start"
 git config --global --add safe.directory /workspaces/verathread-gateway
 git config pull.rebase true
 
+## update hosts file
+echo 127.0.0.1 dev.cluster.local | sudo tee -a /etc/hosts
+echo 127.0.0.1 k3d-local-registry | sudo tee -a /etc/hosts
+
+## add go bin path to rc
+echo 'GOPRIVATE=github.com/azarc-io' >> ~/.zshrc
+echo 'GOPRIVATE=github.com/azarc-io' >> ~/.bashrc
+
 ### clone the vth dev toolkit project
 git clone https://github.com/azarc-io/verathread-dev-toolkit.git /workspaces/verathread-dev-toolkit
 git -C /workspaces/verathread-dev-toolkit pull
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,12 +4,4 @@
 
 echo "post-start start"
 
-## update hosts file
-echo 127.0.0.1 dev.cluster.local | sudo tee -a /etc/hosts
-echo 127.0.0.1 k3d-local-registry | sudo tee -a /etc/hosts
-
-## add go bin path to rc
-echo 'GOPRIVATE=github.com/azarc-io' >> ~/.zshrc
-echo 'GOPRIVATE=github.com/azarc-io' >> ~/.bashrc
-
 echo "post-start complete"
