f057ba66d789a4940e14ba334bb2eed988a26ed6
Wael <wael.jammal@azarc.io>
2024-07-05 14:58:27 +0100

    RPP-6: Dev containers support

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -5,25 +5,25 @@
 echo "on-create start"
 
 ### clone the vth dev toolkit project
-#git clone https://github.com/azarc-io/verathread-dev-toolkit.git /workspaces/verathread-dev-toolkit
+git clone https://github.com/azarc-io/verathread-dev-toolkit.git /workspaces/verathread-dev-toolkit
 #
 ## add go bin path to rc
 #echo 'PATH=$PATH:/go/bin' >> ~/.zshrc
 #echo 'PATH=$PATH:/go/bin' >> ~/.bashrc
-#echo 'GOPRIVATE=github.com/azarc-io' >> ~/.zshrc
-#echo 'GOPRIVATE=github.com/azarc-io' >> ~/.bashrc
+echo 'GOPRIVATE=github.com/azarc-io' >> ~/.zshrc
+echo 'GOPRIVATE=github.com/azarc-io' >> ~/.bashrc
 #export PATH=$PATH:/go/bin
 #
 ## install task.dev
-#go install github.com/go-task/task/v3/cmd/task@latest
-#
+go install github.com/go-task/task/v3/cmd/task@latest
+
 ## run setup task
-#task setup
-#
+task setup
+
 ## update hosts file
-#echo 127.0.0.1 dev.cluster.local | sudo tee -a /etc/hosts
-#echo 127.0.0.1 k3d-local-registry | sudo tee -a /etc/hosts
-#
+echo 127.0.0.1 dev.cluster.local | sudo tee -a /etc/hosts
+echo 127.0.0.1 k3d-local-registry | sudo tee -a /etc/hosts
+
 ## spin up k3d from the toolkit
 #pushd /workspaces/verathread-dev-toolkit
 #task setup
