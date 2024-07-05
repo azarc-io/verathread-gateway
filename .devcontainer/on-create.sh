eb0f0394cd22d47fdf6def098a76c3b0266f5053
Wael <wael.jammal@azarc.io>
2024-07-05 15:27:44 +0100

    RPP-6: Dev containers support

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -10,14 +10,12 @@ git config pull.rebase true
 
 ### clone the vth dev toolkit project
 git clone https://github.com/azarc-io/verathread-dev-toolkit.git /workspaces/verathread-dev-toolkit
-#
+git -C /workspaces/verathread-dev-toolkit pull
+
 ## add go bin path to rc
-#echo 'PATH=$PATH:/go/bin' >> ~/.zshrc
-#echo 'PATH=$PATH:/go/bin' >> ~/.bashrc
 echo 'GOPRIVATE=github.com/azarc-io' >> ~/.zshrc
 echo 'GOPRIVATE=github.com/azarc-io' >> ~/.bashrc
-#export PATH=$PATH:/go/bin
-#
+
 ## install task.dev
 go install github.com/go-task/task/v3/cmd/task@latest
 
@@ -29,9 +27,9 @@ echo 127.0.0.1 dev.cluster.local | sudo tee -a /etc/hosts
 echo 127.0.0.1 k3d-local-registry | sudo tee -a /etc/hosts
 
 ## spin up k3d from the toolkit
-#pushd /workspaces/verathread-dev-toolkit
-#task setup
-#task k3d:create
-#popd
+pushd /workspaces/verathread-dev-toolkit
+task setup
+task k3d:create
+popd
 
 echo "on-create complete"
