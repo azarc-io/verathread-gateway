492d2f7d3d6691b6815caf4f9c8280359da07bc7
Wael <wael.jammal@azarc.io>
2024-07-05 13:59:09 +0100

    RPP-6: Dev containers support

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,23 +11,6 @@ apt-get install -y \
 useradd -m user
 su user
 
-# install go
-VERSION='1.22.2'
-OS='linux'
-ARCH='amd64'
-
-curl -OL https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
-tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
-rm go$VERSION.$OS-$ARCH.tar.gz
-
-INSTALLED_GO_VERSION=$(go version)
-echo "Go version ${INSTALLED_GO_VERSION} is installed"
-
-# set-up and install yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get install yarn -y
-
 # instsal nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
