3f1d0871dba2915ccc8db5144829ad305ab5a062
Wael <wael.jammal@azarc.io>
2024-07-05 12:39:52 +0100

    RPP-6: Dev containers support

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -3,10 +3,30 @@
 # this runs as part of pre-build
 
 echo "on-create start"
-echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create start" >> "$HOME/status"
 
 # clone repos
-git clone https://github.com/cse-labs/imdb-app /workspaces/imdb-app
+git clone https://github.com/azarc-io/verathread-gateway.git /workspaces/verathread-gateway
+
+## install golang
+#wget -c https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
+#sudo tar -C /usr/local/ -xzf go1.22.0.linux-amd64.tar.gz
+## shellcheck disable=SC2016
+## dont want variables to expand
+#echo '
+## set PATH so it includes /usr/local/go/bin if it exists
+#if [ -d "/usr/local/go/bin" ] ; then
+#    PATH="/usr/local/go/bin:$PATH"
+#fi
+#' >> ~/.bashrc
+
+# add go bin path to rc
+echo 'PATH=$PATH:/go/bin' >> ~/.zshrc
+echo 'PATH=$PATH:/go/bin' >> ~/.bashrc
+
+# install task.dev
+go install github.com/go-task/task/v3/cmd/task@latest
+
+# run setup task
+task setup
 
 echo "on-create complete"
-echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create complete" >> "$HOME/status"
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,8 +6,7 @@ echo "post-create start"
 echo "$(date)    post-create start" >> "$HOME/status"
 
 # update the repos
-git -C /workspaces/imdb-app pull
-git -C /workspaces/webvalidate pull
+git -C /workspaces/verathread-gateway pull
 
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
